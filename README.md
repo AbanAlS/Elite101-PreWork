f639f91fb51c03a888ff76851fa7d2f2c847d5cf
AbanA <121648993+AbanAlS@users.noreply.github.com>
2023-02-03 03:51:02 +0000

    A delivery check, and tons of polishing.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Elite101-PreWork
 The pre-work project for the Elite101 class.<br/>
 The pre-work is a python chatbot<br/>
-I'm creating this chatbot for a hamburger restaurant to take orders from customers
+I'm creating this chatbot for a hamburger restaurant to take orders from customers and to check if their order arrived.
--- main.py
+++ main.py
@@ -1,19 +1,70 @@
+import time
+
+# Asks for the user's name
 name = input("Hello, Welcome to Nathaniel's famous burgers! \nName please? ")
-print('\nHi, %s!' % name)
+print('\nHi, ' + name + '!')
 
-def order_number(user_input):
-  numbers = ["1", "2", "3", "4"]
-
-  if user_input in numbers:
-    if user_input == "1":
-      print("One hamburger coming right up!")
-    elif user_input == "2":
-      print("One cheeseburger coming right up!")
-    elif user_input == "3":
-      print("One Double Hamburger coming right up!")
-    elif user_input == "4":
-      print("One Double Cheeseburger coming right up!")
+# Displays the menu and fills out order by number
+def order_number(menu_order):
+  if menu_order == "1":
+    print("One hamburger coming right up!\n")
+    time.sleep(2)
+    print("------------------10 minutes later------------------")
+    time.sleep(2)
+    
+  elif menu_order == "2":
+    print("One cheeseburger coming right up!\n")
+    time.sleep(2)
+    print("------------------10 minutes later------------------")
+    time.sleep(2)
+    
+  elif menu_order == "3":
+    print("One Double Burger coming right up!\n")
+    time.sleep(2)
+    print("------------------10 minutes later------------------")
+    time.sleep(2)
+    
+  elif menu_order == "4":
+    print("One Double Cheeseburger coming right up!\n")
+    time.sleep(2)
+    print("------------------10 minutes later------------------")
+    time.sleep(2)
+    
   else:
-    print('?')
-user_input = input("We have the following Items: \n \n 1. Hamburger \n 2. Cheeseburger \n 3. Double Hamburger \n 4. Double Cheeseburger \n \nWhat number would you like to order? ")
-order_number(user_input)
+    menu_order = input('Come again? ')
+    order_number(menu_order)
+
+# Checks if the order has arrived
+def order_check(order_arrival):
+  if order_arrival == "yes":
+    print("Great! Enjoy your meal!")
+  
+  elif order_arrival == "no":
+    print("Alright, We'll make sure the order arrives on time!\n")
+    time.sleep(2)
+    print("------------------10 minutes later------------------")
+    time.sleep(2)
+    order_remind = input("\nHello, did you recieve your order? ")
+    order_reminder(order_remind)
+    
+  else:
+    order_arrival = input('Come again? ')
+    order_check(order_arrival)
+
+# Checks about the order again
+def order_reminder(order_remind):
+  if order_remind == "yes":
+    print("Good! Sorry for the delay...")
+  elif order_remind == "no":
+    print("Sorry about this. We'll process a refund as soon as possible.")
+  else:
+    order_remind = input("Come again? ")
+    order_reminder(order_remind)
+
+# Menu list
+menu_order = input("We have the following items: \n \n 1. Hamburger \n 2. Cheeseburger \n 3. Double Hamburger \n 4. Double Cheeseburger \n \nWhat number would you like to order? ")
+order_number(menu_order)
+
+# Checks if the order has arrived
+order_arrival = input("\nHi, " + name + "!\nWe're checking in for your order. Did you recieve it yet? ")
+order_check(order_arrival)
